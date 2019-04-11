e77e3a6c65051183ab9640fb280b8950c398a687
Hoa Tran <hoaqt91@gmail.com>
2019-04-11 23:29:45 +0700

    Change variable name

--- src/Solution.java
+++ src/Solution.java
@@ -5,17 +5,17 @@ public class Solution {
 
   public static void main(String args[]) {
     Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-    int n = Integer.parseInt(in.nextLine());
-    for (int i = 1; i <= n; i++) {
+    int t = Integer.parseInt(in.nextLine());
+    for (int index = 1; index <= t; index++) {
       String line = in.nextLine();
       String items[] = line.split(" ");
       // manipulate the item here
-      findSolution(i);
+      findSolution(index);
     }
   }
 
-  public static void findSolution(int i) {
+  public static void findSolution(int index) {
     // Find the solution for each case
-    System.out.println("Case #" + i + ": IMPOSSIBLE");
+    System.out.println("Case #" + index + ": IMPOSSIBLE");
   }
 }
